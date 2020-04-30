e50770ea220d3f06e90fa2a2a25ccaf1f3c84199
Nicolas Pierson <nicolas.pierson57@gmail.com>
2020-04-30 12:14:27 +0200

    carte des soins

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :cares
+  get 'soins', to: 'pages#soins', as: :soins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
